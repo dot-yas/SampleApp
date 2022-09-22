8640ab92864c10da61f79f630d464bca9f62d533
yas <yasuhiro.kato@d2cdot.co.jp>
2022-09-22 14:14:52 +0900

    Initial Commit

---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by yas on 2022/09/22
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
